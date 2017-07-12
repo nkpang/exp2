063aafcd301759bbb0083dd6669e5afa180d21b9
PNK <naikiat5@gmail.com>
2017-07-12 22:23:51 +0800

    added java files

---
+++ MathMain.java
@@ -0,0 +1,12 @@
+
+public class MathMain {
+	
+	//this is to simulate the integration test for Math class
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("9 + 3 = " + Math.add(9,3));
+		System.out.println("9 - 3 = " + Math.minus(9,3));
+		System.out.println("9 * 3 = " + Math.multiply(9,3));
+		System.out.println("9 / 3 = " + Math.divide(9,3));
+	}
+}
