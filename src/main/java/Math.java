9a63b0129cacd0e655cd23ab717bc0adb8725551
nkpang <naikiat5@gmail.com>
2017-07-14 08:13:38 +0800

    added math java

---
+++ src/main/java/Math.java
@@ -0,0 +1,27 @@
+
+public class Math {
+	
+	public Math() {}
+	
+	static public int add(int a, int b) {   
+        return a + b; 
+	}
+	
+	static public int minus(int a, int b) {   
+        return a - b; 
+	}
+	
+	static public int multiply(int a, int b) {   
+        return a * b; 
+	}
+		
+	static public double divide(int a, int b){
+		double c=0.0;
+		try{
+			c= a/b;
+		}catch(Exception e) {
+			throw new IllegalArgumentException();
+		}
+		return c;
+	}
+}
